7a67f3d6d9d208b86a12e223a9ae904a6d816c0d
Carl Spain <neoancient@megamek.org>
2020-08-23 12:56:07 -0500

    Hex coordinate system

---
+++ core/src/main/kotlin/board/HexCoords.kt
@@ -0,0 +1,147 @@
+/**
+ * Copyright (c) 2020 Carl W Spain
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package board
+
+// Direction indices with vertical (flat-topped) orientation
+const val V_DIR_N = 0
+const val V_DIR_NE = 1
+const val V_DIR_SE = 2
+const val V_DIR_S = 3
+const val V_DIR_SW = 4
+const val V_DIR_NW = 5
+
+// Direction indices with horizontal (point-topped) orientation
+const val H_DIR_NW = 0
+const val H_DIR_NE = 1
+const val H_DIR_E = 2
+const val H_DIR_SE = 3
+const val H_DIR_SW = 4
+const val H_DIR_W = 5
+
+/**
+ * A set of coordinates of a hex, with the origin at the top left. Implementations decide orientation
+ * of the hexes and which direction to offset every other row/column.
+ *
+ * @author neoancient
+ */
+abstract class HexCoords(
+    /** The X coordinate of the hex */
+    val row: Int,
+    /** The Y coordinate of the hex */
+    val col: Int) {
+
+    /**
+     * Creates a copy of the coordinates
+     *
+     * @return The copy
+     */
+    abstract fun copy(): HexCoords
+
+    /**
+     * @return The x coordinate of the hex in a rectangular coordinate system
+     */
+    abstract fun cartesianX(): Double
+
+    /**
+     * @return The y coordinate of the hex in a rectangular coordinate system
+     */
+    abstract fun cartesianY(): Double
+
+    /**
+     * Calculates the range to another hex
+     *
+     * @param other  The other hex
+     * @return       The range in hexes
+     */
+    abstract fun distance(other: HexCoords): Int
+
+    /**
+     * Computes the direction from this hex to another. If the coordinates are the
+     * same, returns 0 as a default value.
+     *
+     * @param coords        The other hex
+     * @return The direction in radians, with zero at the top of the map and proceeding clockwise.
+     */
+    abstract fun directionTo(coords: HexCoords): Double
+
+    /**
+     * The direction to another hex in degrees, rounded normally. If the coordinates are the same,
+     * returns 0.
+     *
+     * @param coords The other hex
+     * @return       The direction in degrees, with zero at the top of the map and proceeding clockwise.
+     */
+    abstract fun degreesTo(coords: HexCoords): Int
+
+    /**
+     * Computes the direction to another hex relative to a given facing rather than relative to the top of
+     * the board.
+     *
+     * @param facing The reference facing
+     * @param coords The other hex
+     * @return The direction in radians, with 0 being the same as the reference facing.
+     */
+    abstract fun relativeBearing(facing: Int, coords: HexCoords): Double
+
+    /**
+     * Computes the direction to another hex relative to a given facing rather than relative to the top of
+     * the board.
+     *
+     * @param facing The reference facing
+     * @param coords The other hex
+     * @return The direction in degrees, with 0 being the same as the reference facing.
+     */
+    abstract fun relativeBearingDegrees(facing: Int, coords: HexCoords): Int
+
+    /**
+     * Finds the adjacent hex in a given direction.
+     * @param direction The direction to move
+     * @return The coordinates of the adjacent hex.
+     */
+    abstract fun adjacent(direction: Int): HexCoords
+
+    /**
+     * Translates the hex in the given direction.
+     * @param direction The direction to move.
+     * @return          The hex in the direction to translate
+     */
+    abstract fun translate(direction: Int): HexCoords
+
+    /**
+     * Translates the hex a number of columns and rows
+     *
+     * @param dCol The number of columns to move the hex
+     * @param dRow The number of rows to move the hex
+     * @return     The hex in the given direction
+     */
+    abstract fun translate(dCol: Int, dRow: Int): HexCoords
+
+    /**
+     * Rotates the hex by a multiple of 60 degrees around a center hex.
+     *
+     * @param change The number of 60 degree angles to rotate. Positive for clockwise and negative
+     * for anticlockwise
+     * @param center The hex at the center of the rotation.
+     * @return       The hex in the rotated position
+     */
+    abstract fun rotate(change: Int, center: HexCoords): HexCoords
+}
---
+++ core/src/test/kotlin/board/AxialCoordsTest.kt
@@ -0,0 +1,254 @@
+package board
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.function.Executable
+
+/**
+ *
+ */
+internal class AxialCoordsTest {
+
+    @Test
+    fun sameColumnForSameXVertical() {
+        val coords1 = AxialCoords.createFromOffset(3, 3, vertical = true, offsetOdd = true)
+        val coords2 = AxialCoords.createFromOffset(3, 5, vertical = true, offsetOdd = true)
+        assertEquals(coords1.col, coords2.col)
+    }
+
+    @Test
+    fun sameRowForSameYHorizontal() {
+        val coords1 = AxialCoords.createFromOffset(3, 3, vertical = false, offsetOdd = true)
+        val coords2 = AxialCoords.createFromOffset(5, 3, vertical = false, offsetOdd = true)
+        assertEquals(coords1.row, coords2.row)
+    }
+
+    @Test
+    fun testOddColumnOffset() {
+        val evenColumn = AxialCoords.createFromOffset(0, 0, vertical = true, offsetOdd = true)
+        val oddColumn = AxialCoords.createFromOffset(1, 0, vertical = true, offsetOdd = true)
+        assertTrue(evenColumn.cartesianY() < oddColumn.cartesianY())
+    }
+
+    @Test
+    fun testEvenColumnOffset() {
+        val evenColumn = AxialCoords.createFromOffset(0, 0, vertical = true, offsetOdd = false)
+        val oddColumn = AxialCoords.createFromOffset(1, 0, vertical = true, offsetOdd = false)
+        assertTrue(evenColumn.cartesianY() > oddColumn.cartesianY())
+    }
+
+    @Test
+    fun testOddRowOffset() {
+        val evenRow = AxialCoords.createFromOffset(0, 0, vertical = false, offsetOdd = true)
+        val oddRow = AxialCoords.createFromOffset(0, 1, vertical = false, offsetOdd = true)
+        assertTrue(evenRow.cartesianX() < oddRow.cartesianX())
+    }
+
+    @Test
+    fun testEvenRowOffset() {
+        val evenRow = AxialCoords.createFromOffset(0, 0, vertical = false, offsetOdd = false)
+        val oddRow = AxialCoords.createFromOffset(0, 1, vertical = false, offsetOdd = false)
+        assertTrue(evenRow.cartesianX() > oddRow.cartesianX())
+    }
+
+
+    @Test
+    fun distanceToSameHexIsZero() {
+        val coords = AxialCoords(8, 8, true)
+        assertEquals(coords.distance(coords), 0)
+    }
+
+    @Test
+    fun distanceInSameColumnVerticalIsDifferenceInY() {
+        val coords1 = AxialCoords.createFromOffset(8, 8, vertical = true, offsetOdd = true)
+        val coords2 = AxialCoords.createFromOffset(8, 6, vertical = true, offsetOdd = true)
+        assertAll(
+                Executable { assertEquals(coords1.distance(coords2), 2) },
+                Executable { assertEquals(coords2.distance(coords1), 2) }
+        )
+    }
+
+    @Test
+    fun distanceInSameRowHorizontalIsDifferenceInX() {
+        val coords1 = AxialCoords.createFromOffset(8, 8, vertical = false, offsetOdd = true)
+        val coords2 = AxialCoords.createFromOffset(6, 8, vertical = false, offsetOdd = true)
+        assertAll(
+                Executable { assertEquals(coords1.distance(coords2), 2) },
+                Executable { assertEquals(coords2.distance(coords1), 2) }
+        )
+    }
+
+    @Test
+    fun testDistanceNonStraightLine() {
+        val coords1 = AxialCoords(8, 8, true)
+        val coords2 = AxialCoords(coords1)
+                .translate(1)
+                .translate(1)
+                .translate(0)
+        assertAll(
+                Executable { assertEquals(coords1.distance(coords2), 3) },
+                Executable { assertEquals(coords2.distance(coords1), 3) }
+        )
+    }
+
+    @Test
+    fun directionFromSameHexIsZeroDegreesWithVerticalOrientation() {
+        val coords1 = AxialCoords(8, 8, true)
+        val coords2 = AxialCoords(8, 8, true)
+        assertEquals(coords1.degreesTo(coords2), 0)
+    }
+
+    @Test
+    fun directionFromSameHexIsZeroDegreesWithHorizontalOrientation() {
+        val coords1 = AxialCoords(8, 8, false)
+        val coords2 = AxialCoords(8, 8, false)
+        assertEquals(coords1.degreesTo(coords2), 0)
+    }
+
+    @Test
+    fun directionToHexAboveIsZeroDegreesWithVerticalOrientation() {
+        val coords1 = AxialCoords(8, 8, true)
+        val coords2 = AxialCoords(coords1)
+        coords2.translate(V_DIR_N)
+        coords2.translate(V_DIR_N)
+        assertEquals(coords1.degreesTo(coords2), 0)
+    }
+
+    @Test
+    fun directionToHexAboveIsZeroDegreesWithHorizontalOrientation() {
+        val coords1 = AxialCoords(8, 8, false)
+        val coords2 = AxialCoords(coords1)
+        coords2.translate(H_DIR_NE)
+        coords2.translate(H_DIR_NW)
+        assertEquals(coords1.degreesTo(coords2), 0)
+    }
+
+    @Test
+    fun degreesToHexBelowIs180DegreesWithVerticalOrientation() {
+        val coords1 = AxialCoords(8, 8, true)
+        val coords2 = AxialCoords(coords1)
+                .translate(V_DIR_S)
+                .translate(V_DIR_S)
+        assertEquals(coords1.degreesTo(coords2), 180)
+    }
+
+    @Test
+    fun degreesToHexBelowIs180DegreesWithHorizontalOrientation() {
+        val coords1 = AxialCoords(8, 8, false)
+        val coords2 = AxialCoords(coords1)
+                .translate(H_DIR_SE)
+                .translate(H_DIR_SW)
+        assertEquals(coords1.degreesTo(coords2), 180)
+    }
+
+    @Test
+    fun degreesToHexToRightIs90DegreesWithVerticalOrientation() {
+        val coords1 = AxialCoords(8, 8, true)
+        val coords2 = AxialCoords(coords1)
+                .translate(V_DIR_NE)
+                .translate(V_DIR_SE)
+        assertEquals(coords1.degreesTo(coords2), 90)
+    }
+
+    @Test
+    fun degreesToHexToRightIs90DegreesWithHorizontalOrientation() {
+        val coords1 = AxialCoords(8, 8, false)
+        val coords2 = AxialCoords(coords1)
+                .translate(H_DIR_E)
+                .translate(H_DIR_E)
+        assertEquals(coords1.degreesTo(coords2), 90)
+    }
+
+    @Test
+    fun degreesToHexToLeftIs270DegreesWithVerticalOrientation() {
+        val coords1 = AxialCoords(8, 8, true)
+        val coords2 = AxialCoords(coords1)
+                .translate(V_DIR_NW)
+                .translate(V_DIR_SW)
+        assertEquals(coords1.degreesTo(coords2), 270)
+    }
+
+    @Test
+    fun degreesToHexToLeftIs270DegreesWithHorizontalOrientation() {
+        val coords1 = AxialCoords(8, 8, false)
+        val coords2 = AxialCoords(coords1)
+                .translate(H_DIR_W)
+                .translate(H_DIR_W)
+        assertEquals(coords1.degreesTo(coords2), 270)
+    }
+
+    @Test
+    fun relativeBearingFromNEToNWIs240() {
+        val coords1 = AxialCoords(8, 8, true)
+        val coords2 = AxialCoords(coords1).translate(V_DIR_NW)
+        assertEquals(coords1.relativeBearingDegrees(V_DIR_NE, coords2), 240)
+    }
+
+    @Test
+    fun relativeBearingFromNWToNEIs120() {
+        val coords1 = AxialCoords(8, 8, true)
+        val coords2 = AxialCoords(coords1).translate(V_DIR_NE)
+        assertEquals(coords1.relativeBearingDegrees(V_DIR_NW, coords2), 120)
+    }
+
+    @Test
+    fun rotatePositiveSteps() {
+        val center = AxialCoords(8, 8, true)
+        val testHex = AxialCoords(center)
+                .translate(1)
+                .translate(1)
+                .rotate(2, center)
+        assertEquals(center.degreesTo(testHex), 180)
+    }
+
+    @Test
+    fun rotatePositiveStepsPast180() {
+        val center = AxialCoords(8, 8, true)
+        val testHex = AxialCoords(center)
+                .translate(1)
+                .translate(1)
+                .rotate(4, center)
+        assertEquals(center.degreesTo(testHex), 300)
+    }
+
+    @Test
+    fun rotateZero() {
+        val center = AxialCoords(8, 8, true)
+        val testHex = AxialCoords(center)
+                .translate(1)
+                .translate(1)
+                .rotate(0, center)
+        assertEquals(center.degreesTo(testHex), 60)
+    }
+
+    @Test
+    fun rotateMoreThanSixSteps() {
+        val center = AxialCoords(8, 8, true)
+        val testHex = AxialCoords(center)
+                .translate(1)
+                .translate(1)
+                .rotate(7, center)
+        assertEquals(center.degreesTo(testHex), 120)
+    }
+
+    @Test
+    fun rotateNegativeSteps() {
+        val center = AxialCoords(8, 8, true)
+        val testHex = AxialCoords(center)
+                .translate(1)
+                .translate(1)
+                .rotate(-2, center)
+        assertEquals(center.degreesTo(testHex), 300)
+    }
+
+    @Test
+    fun rotateMoreThanSixNegativeSteps() {
+        val center = AxialCoords(8, 8, true)
+        val testHex = AxialCoords(center)
+                .translate(1)
+                .translate(1)
+                .rotate(-9, center)
+        assertEquals(center.degreesTo(testHex), 240)
+    }
+
+}
