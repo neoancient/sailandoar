a5705fc1ccfb6d068c523123169ec23b46d44df5
Carl Spain <neoancient@megamek.org>
2020-08-25 15:05:57 -0500

    Added serialization for ShipStats

---
+++ core/src/test/kotlin/unit/ShipStatsTest.kt
@@ -0,0 +1,23 @@
+package unit
+
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.util.*
+
+internal class ShipStatsTest {
+    @Test
+    fun testSerialization() {
+        val ship = ShipStats(UUID.randomUUID(), "Irish Rover",
+            SizeClass.VERY_LARGE, 10000000, RiggingType.FORE_AND_AFT,
+            23, 6, 1, 2, false, 0, 0,
+            0, RamType.NONE, 400, 200, 0, 8, 0, 0)
+
+        val json = Json.encodeToString(ship)
+        val decoded = Json.decodeFromString<ShipStats>(json)
+
+        assertEquals(ship, decoded)
+    }
+}
