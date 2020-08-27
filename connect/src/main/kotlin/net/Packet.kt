1b5dfe71f5df1d1add1e10bad34545139a79b552
Carl Spain <neoancient@megamek.org>
2020-08-27 10:39:13 -0500

    Added connection module and Packet class.

---
+++ connect/src/main/kotlin/net/Packet.kt
@@ -0,0 +1,56 @@
+/**
+ * Sail and Oar
+ * Copyright (c) 2020 Carl W Spain
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net
+
+import game.Game
+import game.Player
+import kotlinx.serialization.Serializable
+import unit.BaseUnit
+import unit.ShipStats
+
+@Serializable
+sealed class Packet {
+    abstract val clientId: Int
+}
+
+@Serializable
+class InitClientPacket(override val clientId: Int, val game: Game): Packet()
+
+@Serializable
+class AddPlayerPacket(override val clientId: Int, val player: Player): Packet()
+
+@Serializable
+class RemovePlayerPacket(override val clientId: Int, val player: Player): Packet()
+
+@Serializable
+class RequestAvailableShipsPacket(override val clientId: Int): Packet()
+
+@Serializable
+class SendAvailableShipsPacket(override val clientId: Int, val ships: Collection<ShipStats>): Packet()
+
+@Serializable
+class AddUnitPacket(override val clientId: Int, val unit: BaseUnit): Packet()
+
+@Serializable
+class RemoveUnitPacket(override val clientId: Int, val unit: BaseUnit): Packet()
---
+++ connect/src/test/kotlin/net/PacketTest.kt
@@ -0,0 +1,64 @@
+/**
+ * Sail and Oar
+ * Copyright (c) 2020 Carl W Spain
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net
+
+import game.Player
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromByteArray
+import kotlinx.serialization.encodeToByteArray
+import kotlinx.serialization.protobuf.ProtoBuf
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+@ExperimentalSerializationApi
+internal class PacketTest {
+    @Test
+    fun serializePacket() {
+        val packet: Packet = RequestAvailableShipsPacket(1)
+
+        val bytes = ProtoBuf.encodeToByteArray(packet)
+        val decoded = ProtoBuf.decodeFromByteArray<Packet>(bytes)
+
+        assertAll(
+                {assertEquals(packet.clientId, decoded.clientId)},
+                {assertTrue(decoded is RequestAvailableShipsPacket)}
+        )
+    }
+
+    @Test
+    fun serializePacketWithData() {
+        val player = Player(2, "Test Player")
+        val packet: Packet = AddPlayerPacket(3, player)
+
+
+        val bytes = ProtoBuf.encodeToByteArray(packet)
+        val decoded = ProtoBuf.decodeFromByteArray<Packet>(bytes)
+
+        assertAll(
+                {assertEquals(packet.clientId, decoded.clientId)},
+                {assertEquals(player.id, (packet as AddPlayerPacket).player.id)},
+                {assertEquals(player.name, (packet as AddPlayerPacket).player.name)}
+        )
+    }
+}
