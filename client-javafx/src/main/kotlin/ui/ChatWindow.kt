6d74b1030e0364d1a348600ed14a6978094c7ff4
Carl Spain <neoancient@megamek.org>
2021-01-26 15:45:00 -0600

    Extracted chat window to its own class

---
+++ client-javafx/src/main/kotlin/ui/ChatWindow.kt
@@ -0,0 +1,94 @@
+/*
+ * Sail and Oar
+ * Copyright (c) 2021 Carl W Spain
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package ui
+
+import game.GameListener
+import javafx.application.Platform
+import javafx.concurrent.Worker
+import javafx.fxml.FXML
+import javafx.scene.control.Button
+import javafx.scene.control.TextField
+import javafx.scene.layout.VBox
+import javafx.scene.web.WebView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import tornadofx.*
+
+class ChatWindow : GameListener, View() {
+    override val root: VBox by fxml()
+
+    private val txtChatWindow: WebView by fxid()
+    private val txtChatEntry: TextField by fxid()
+    private val btnSend: Button by fxid()
+
+    private val chatMessages = StringBuilder()
+
+    private var chatWindowAtBottom = true
+
+    init {
+        btnSend.enableWhen(txtChatEntry.textProperty().isNotEmpty)
+        (app as SailAndOarApp).clientProperty.value?.game?.addListener(this)
+        txtChatWindow.engine.loadWorker.stateProperty().onChange {
+            it?.let { state ->
+                if (state == Worker.State.SUCCEEDED && chatWindowAtBottom) {
+                    txtChatWindow.engine.executeScript("window.scrollTo(0, document.body.scrollHeight)")
+                }
+            }
+        }
+    }
+
+    @FXML
+    private fun btnSendAction() {
+        GlobalScope.launch(Dispatchers.IO) {
+            (app as SailAndOarApp).clientProperty.value?.sendChatMessage(txtChatEntry.text)
+            Platform.runLater {
+                txtChatEntry.text = ""
+            }
+        }
+    }
+
+    override fun appendChat(text: String) {
+        Platform.runLater {
+            chatMessages.append(text)
+            with (txtChatWindow.engine) {
+                chatWindowAtBottom = executeScript("(window.innerHeight + window.scrollY) >= document.body.offsetHeight") as Boolean
+                loadContent(chatMessages.toString())
+            }
+        }
+    }
+
+    override fun playerAdded(playerId: Int) {
+    }
+
+    override fun playerRemoved(playerId: Int) {
+    }
+
+    override fun unitAdded(unitId: Int) {
+    }
+
+    override fun unitRemoved(unitId: Int) {
+    }
+}
--- client-javafx/src/main/kotlin/ui/MainUI.kt
+++ client-javafx/src/main/kotlin/ui/MainUI.kt
@@ -14,27 +14,14 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import tornadofx.*
 
-class MainUI: GameListener, View() {
+class MainUI: View() {
+    val chatWindow: ChatWindow by inject()
+
     override val root: BorderPane by fxml()
     private val panCenter: StackPane by fxid()
-    private val txtChatWindow: WebView by fxid()
-    private val txtChatEntry: TextField by fxid()
-    private val btnSend: Button by fxid()
-
-    private val chatMessages = StringBuilder()
-
-    private var chatWindowAtBottom = true
 
     init {
-        btnSend.enableWhen(txtChatEntry.textProperty().isNotEmpty)
-        (app as SailAndOarApp).clientProperty.value?.game?.addListener(this)
-        txtChatWindow.engine.loadWorker.stateProperty().onChange {
-            it?.let { state ->
-                if (state == Worker.State.SUCCEEDED && chatWindowAtBottom) {
-                    txtChatWindow.engine.executeScript("window.scrollTo(0, document.body.scrollHeight)")
-                }
-            }
-        }
+        root.bottom = chatWindow.root
     }
 
     @FXML
@@ -46,41 +33,9 @@ class MainUI: GameListener, View() {
     @FXML
     private fun menuHelpAbout() {}
 
-    @FXML
-    private fun btnSendAction() {
-        GlobalScope.launch(Dispatchers.IO) {
-            (app as SailAndOarApp).clientProperty.value?.sendChatMessage(txtChatEntry.text)
-            Platform.runLater {
-                txtChatEntry.text = ""
-            }
-        }
-    }
-
     override fun onDock() {
         currentWindow?.setOnCloseRequest {
             (app as SailAndOarApp).close()
-        }
-    }
-
-    override fun playerAdded(playerId: Int) {
-    }
-
-    override fun playerRemoved(playerId: Int) {
-    }
-
-    override fun unitAdded(unitId: Int) {
-    }
-
-    override fun unitRemoved(unitId: Int) {
-    }
-
-    override fun appendChat(text: String) {
-        Platform.runLater {
-            chatMessages.append(text)
-            with (txtChatWindow.engine) {
-                chatWindowAtBottom = executeScript("(window.innerHeight + window.scrollY) >= document.body.offsetHeight") as Boolean
-                loadContent(chatMessages.toString())
-            }
         }
     }
 }
